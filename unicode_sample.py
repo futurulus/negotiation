2cf6ecc9912d8721b3abd30706f554779b433e17
Will Monroe <wmonroe4@cs.stanford.edu>
2018-01-08 23:46:09 -0800

    Fix GPU assignment in Unicode sampling script

--- unicode_sample.py
+++ unicode_sample.py
@@ -1,16 +1,18 @@
+'''
+python unicode_sample.py --load runs/unicode_simple/model.pkl --device gpu1
+'''
 import pickle
 import sys
 import json
 
+from stanza.research import config
 from stanza.research.instance import Instance
 
+import thutils
 from simple import SimpleSeq2SeqLearner
 
 
-def sample_unicode(model_pkl_file):
-    with open(model_pkl_file, 'rb') as infile:
-        model = pickle.load(infile)
-
+def sample_unicode(model_pkl_file, device):
     dev_insts = []
     with open('data/unicode_dev.json', 'r') as infile:
         for line in infile:
@@ -19,11 +21,17 @@ def sample_unicode(model_pkl_file):
                 dev_insts.append(Instance(**json.loads(line)))
     dev_insts = dev_insts[:256]
 
-    for inst, sample in zip(dev_insts, model.predict(dev_insts, random=True)):
+    with thutils.device_context(device):
+        with open(model_pkl_file, 'rb') as infile:
+            model = pickle.load(infile)
+
+        samples = model.predict(dev_insts, random=True)
+
+    for inst, sample in zip(dev_insts, samples):
         char = chr(int(inst.input, 16))
         print(f'{char} U+{inst.input} {sample}')
 
 
 if __name__ == '__main__':
-    model_pkl_file = sys.argv[1]
-    sample_unicode(model_pkl_file)
+    options = config.options()
+    sample_unicode(options.load, options.device)
